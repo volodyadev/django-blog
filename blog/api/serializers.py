0771806fdf37d08c4bffc4281896afb868100ac9
Volodya Petrov <v.petrov@acomps.ru>
2024-09-13 11:50:49 +0300

    Created API endpoints for posts.

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'body', 'owner']
 
 
 class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'posts']
+
+
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
-from . import serializers
 from django.contrib.auth.models import User
+
+from . import serializers
+from .models import Post
 
 
 class UserList(generics.ListAPIView):
@@ -11,3 +13,16 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
