e0e0b8805a447f4a0fb3c520bef53d29e10f2f4a
Volodya Petrov <v.petrov@acomps.ru>
2024-09-13 12:12:05 +0300

    Created a model for comments.

--- blog/api/models.py
+++ blog/api/models.py
@@ -11,3 +11,15 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["created"]
+
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(blank=False)
+    owner = models.ForeignKey(
+        "auth.User", related_name="comments", on_delete=models.CASCADE
+    )
+    post = models.ForeignKey("Post", related_name="comments", on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["created"]
